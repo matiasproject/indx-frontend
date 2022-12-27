0dd7b5f8c18579cba0ab4ad24cd19a2ae3df9601
Matías Delgado Ródenas <mdelgado@fintonic.com>
2022-12-27 18:41:45 +0100

    feat(base-app): add root project and App with providers: styled-components, React Query and React router

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider as Styled } from 'styled-components';
+import StyledGlobal from 'styles/styled-global';
+import styledTheme from 'styles/styled-theme';
+import { routes } from 'pages';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <StyledGlobal />
+      <Styled theme={styledTheme}>
+        <RouterProvider router={routes} />
+      </Styled>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}

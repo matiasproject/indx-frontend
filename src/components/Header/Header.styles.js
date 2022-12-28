9d685137e6eb084cb2c93dc5e90d28d16da67819
Matías Delgado Ródenas <mdelgado@fintonic.com>
2022-12-28 13:07:20 +0100

    feat(base-app): styled Header component and logic state loading

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,3 +1,13 @@
 import styled from 'styled-components';
+import { rem } from 'polished';
 
-export const StyledHeader = styled.div``;
+export const StyledHeader = styled.header`
+    font-size: ${({ theme }) => theme.sizes.title};
+    font-weight: 600;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.lightGray};
+    color: ${({ theme }) => theme.colors.blue};
+    padding: ${rem('20px')};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;

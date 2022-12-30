54388dc27b038e92a94d43f9c337155acff93341
Matías Delgado Ródenas <mdelgado@fintonic.com>
2022-12-30 14:14:28 +0100

    feat(PodcastBox): add logic for Podcast Item

--- src/components/PodcastBox/PodcastBox.component.js
+++ src/components/PodcastBox/PodcastBox.component.js
@@ -1,9 +1,26 @@
+import { Link } from 'components';
 import { StyledPodcastBox } from './PodcastBox.styles';
 
-export const PodcastBox = () => {
+export const PodcastBox = ({podcast}) => {
+  const {
+    id: { attributes: { 'im:id': podcastId} },
+    'im:name': {label: podcastName},
+    'im:artist': {label: artist},
+    'im:image': image,
+  } = podcast;
+
+  const { label: imageUrl } = image[2];
+  const title = podcastName.toUpperCase();
+
   return (
     <StyledPodcastBox>
-      <span>PodcastBox</span>
+      <Link to={`/podcast/${podcastId}`}>
+        <img src={imageUrl} alt={title} />
+        <div>
+          <h3 title={title}>{title}</h3>
+          <p>Author: {artist}</p>
+        </div>
+      </Link>
     </StyledPodcastBox>
   );
 };
--- src/components/PodcastBox/PodcastBox.styles.js
+++ src/components/PodcastBox/PodcastBox.styles.js
@@ -1,3 +1,47 @@
 import styled from 'styled-components';
 
-export const StyledPodcastBox = styled.div``;
+export const StyledPodcastBox = styled.section`
+  text-align: center;
+  min-width: 277px;
+  transition: all .2s ease-in-out;
+  &:hover { transform: scale(1.03); }
+
+  img {
+    border-radius: 100px;
+    margin-bottom: 20px;
+    z-index: 1;
+    width: 150px;
+    height: 150px;
+    border: 2px solid ${({ theme }) => theme.colors.lightGray};
+  }
+  a {
+    display: block;
+    color: inherit;
+  }
+  div {
+    margin-top: -105px;
+    box-shadow: 1px 1px 11px 1px rgb(224 224 224 / 73%);
+    padding: 100px 20px 20px;
+    transition: all .2s ease-in-out;
+  }
+  h3 {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    font-size: ${({ theme }) => theme.sizes.subTitle};
+    linet-height: ${({ theme }) => theme.lineHeight.subTitle};
+    color: ${({ theme }) => theme.colors.black};
+    margin-bottom: 10px;
+  }
+  p {
+    font-size: ${({ theme }) => theme.sizes.paragraph};
+    line-height: ${({ theme }) => theme.lineHeight.paragraph};
+    color: ${({ theme }) => theme.colors.gray};
+    overflow:hidden; 
+    text-overflow:ellipsis;
+    display:-webkit-box; 
+    -webkit-box-orient:vertical;
+    -webkit-line-clamp:2;
+    min-height: 40px;
+  }
+`;

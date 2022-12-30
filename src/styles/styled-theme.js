e966208d46e46783edc607acc23878ea7eca646e
Matías Delgado Ródenas <mdelgado@fintonic.com>
2022-12-30 14:19:31 +0100

    feat(styled-theme): add new properties for theme css

--- src/styles/styled-theme.js
+++ src/styles/styled-theme.js
@@ -5,13 +5,19 @@ const theme = {
     white: '#ffffff',
     blue: '#2b86c2',
     black: '#190f06',
-    gray: '#9595958',
+    gray: '#808080',
+    mediumGray: '#c5c5c5',
     lightGray: '#efefef',
   },
   sizes: {
-    title: `${rem('22px')}`,
+    title: `${rem('28px')}`,
     subTitle: `${rem('18px')}`,
     paragraph: `${rem('16px')}`,
+  },
+  lineHeight: {
+    title: `${rem('31px')}`,
+    subTitle: `${rem('21px')}`,
+    paragraph: `${rem('19px')}`,
   },
 };
 

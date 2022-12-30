5dbe8e2b5c64c4b321854378e7a4852fad6b6d7e
Matías Delgado Ródenas <mdelgado@fintonic.com>
2022-12-30 14:16:03 +0100

    chore(constants): refactor constants/api

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,2 @@
 export const corsHerokuUrl = process.env.REACT_APP_CORS_HEROKU;
 export const itunesPodcastUrl = process.env.REACT_APP_ITUNES_TOP_PODCAST;
-export const services = {
-  getPodcast: 'getPodcast',
-  getPodcastById: 'getPodcastById',
-};

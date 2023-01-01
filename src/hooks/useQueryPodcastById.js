7a7ccd7baa695b1f3ad63fb01ed331947527d35d
Matías Delgado Ródenas <mdelgado@fintonic.com>
2023-01-01 23:40:45 +0100

    feat(podcast-episode): added filter in useQueryPodCastById for get track episode

--- src/hooks/useQueryPodcastById.js
+++ src/hooks/useQueryPodcastById.js
@@ -2,7 +2,7 @@ import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { oneDayInMilliseconds } from 'constants';
 import * as api from 'api';
 
-export function useQueryPodcastById(id) {
+export function useQueryPodcastById(id, episodeId = '') {
   const queryClient = useQueryClient();
 
   const { isLoading, error, data } = useQuery(
@@ -11,7 +11,17 @@ export function useQueryPodcastById(id) {
       keepPreviousData: true,
       staleTime: oneDayInMilliseconds,
       initialData: () => queryClient.getQueryData(['getPodcastById', id]),
-    }
+      select: tracks => {
+        const { resultCount, results } = tracks;
+        const sliceResults = results.slice(1);
+
+        return {
+          resultCount,
+          results: episodeId ? sliceResults.find(({trackId}) => trackId === Number(episodeId)) : sliceResults,
+        }
+      },
+    },
   );
+
   return { isLoading, error, data };
 }

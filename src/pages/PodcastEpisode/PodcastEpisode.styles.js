016f47a3d66cbaa7ab00fce3fb9aec3c4963f6e4
Matías Delgado Ródenas <mdelgado@fintonic.com>
2023-01-01 23:38:36 +0100

    feat(podcast-episode): finished PodcastBoxEpisode page

--- src/pages/PodcastEpisode/PodcastEpisode.styles.js
+++ src/pages/PodcastEpisode/PodcastEpisode.styles.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
+import {
+  StyledPodcastDetail,
+  StyledLoading,
+} from 'pages/PodcastDetail/PodcastDetail.styles';
 
-export const StyledPodcastEpisode = styled.section`
-  display: grid;
-  grid-template-columns: 300px 1fr;
-  grid-template-rows: repeat(5, 1fr);
-  grid-column-gap: 74px;
-  grid-row-gap: 0px;
-`;
+export const StyledPodcastEpisode = styled(StyledPodcastDetail)``;
+export const StyledLoadingEpisode = styled(StyledLoading)``;
--- src/pages/PodcastEpisode/PodcastEpisode.view.js
+++ src/pages/PodcastEpisode/PodcastEpisode.view.js
@@ -1,10 +1,34 @@
-import { useQueryPodcastEpisode } from 'hooks';
 import { useParams } from 'react-router-dom';
+import { MainLayout } from 'layouts';
+import * as Component from 'components';
+import { useQueryPodcastById, useQueryPodcastEpisode } from 'hooks';
+import { StyledPodcastEpisode, StyledLoadingEpisode } from './PodcastEpisode.styles';
 
 export const PodcastEpisodePage = () => {
+  window.scrollTo(0, 0);
   const { podcastId, episodeId } = useParams();
-  const { data } = useQueryPodcastEpisode(podcastId);
+  const { data: track } = useQueryPodcastById(podcastId, episodeId);
+  const { data: podcastDetail } = useQueryPodcastEpisode(podcastId);
 
-  console.log(data, podcastId, episodeId);
-  return (<h1>EPISODE</h1>);
+  return (
+    <MainLayout>
+      {
+        (track && podcastDetail)
+          ? (
+            <StyledPodcastEpisode>
+              <section>
+                <Component.PodcastBoxDetail podcast={podcastDetail} />
+              </section>
+              <section>
+                <Component.PodcastBoxEpisode track={track} />
+              </section>
+            </StyledPodcastEpisode>
+          ) : (
+            <StyledLoadingEpisode>
+              <p>Cargando...</p>
+            </StyledLoadingEpisode>
+          )
+      }
+    </MainLayout>
+  );
 };

488878adf8ad9bd7718d5b132202976c73d5fe02
Matías Delgado Ródenas <mdelgado@fintonic.com>
2023-01-01 23:38:07 +0100

    feat(podcast-episode): finished PodcastBoxEpisode

--- src/components/PodcastBoxEpisode/PodcastBoxEpisode.component.js
+++ src/components/PodcastBoxEpisode/PodcastBoxEpisode.component.js
@@ -1,9 +1,19 @@
 import { StyledPodcastBoxEpisode } from './PodcastBoxEpisode.styles';
+import { Description } from 'components';
 
-export const PodcastBoxEpisode = () => {
+export const PodcastBoxEpisode = ({track}) => {
+  const {
+    results: {
+      trackName,
+      description,
+      episodeUrl,
+    }} = track;
+
   return (
     <StyledPodcastBoxEpisode>
-      <span>PodcastBoxEpisode</span>
+      <h3>{trackName}</h3>
+      <Description text={description}/>
+      <audio src={episodeUrl} controls />
     </StyledPodcastBoxEpisode>
   );
 };
--- src/components/PodcastBoxEpisode/PodcastBoxEpisode.styles.js
+++ src/components/PodcastBoxEpisode/PodcastBoxEpisode.styles.js
@@ -1,3 +1,26 @@
 import styled from 'styled-components';
 
-export const StyledPodcastBoxEpisode = styled.div``;
+export const StyledPodcastBoxEpisode = styled.section`
+  box-shadow: 1px 1px 11px 1px rgb(224 224 224 / 73%);
+  padding: 20px;
+  background-color: ${({ theme }) => theme.colors.white};
+  box-sizing: border-box;
+
+  h3 {
+    font-size: ${({ theme }) => theme.sizes.subTitle};
+    line-height: ${({ theme }) => theme.lineHeight.subTitle};
+    color: ${({ theme }) => theme.colors.black};
+    margin-bottom: 20px;
+    word-wrap: break-word;
+    font-weight: 600;
+  }
+  p {
+    font-size: ${({ theme }) => theme.sizes.paragraph};
+    line-height: ${({ theme }) => theme.lineHeight.paragraph};
+    color: ${({ theme }) => theme.colors.gray};
+    margin-bottom: 40px;
+  }
+  audio {
+    width: 100%;
+  }
+`;

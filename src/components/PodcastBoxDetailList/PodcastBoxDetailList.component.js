5b9d9305d6ab05139a8749c9e9178910b1aad899
Matías Delgado Ródenas <mdelgado@fintonic.com>
2023-01-01 16:50:50 +0100

    feat(podcast-detail): finish PodcastBoxDetailList component

--- src/components/PodcastBoxDetailList/PodcastBoxDetailList.component.js
+++ src/components/PodcastBoxDetailList/PodcastBoxDetailList.component.js
@@ -1,9 +1,41 @@
 import { StyledPodcastBoxDetailList } from './PodcastBoxDetailList.styles';
+import { Link } from 'components';
+import { formatDate, convertMsToTime } from 'utils';
 
-export const PodcastBoxDetailList = () => {
+export const PodcastBoxDetailList = ({trackList}) => {
+  if (!trackList) {
+    return null;
+  }
+
+  const { resultCount, results } = trackList;
+  const trackResult = resultCount - 1;
+  const tracks = results ?? [];
+
   return (
     <StyledPodcastBoxDetailList>
-      <span>PodcastBoxDetailList</span>
+      <header>
+        <h2>Episodes: {trackResult}</h2>
+      </header>
+      <section>
+        <header>
+          <span>Title</span>
+          <span>Date</span>
+          <span>Duration</span>
+        </header>
+        {
+          tracks.map(track => (
+            <div key={track.trackId}>
+              <span>
+                <Link to={`/podcast/${track.collectionId}/episode/${track.trackId}`}>
+                  {track.trackName}
+                </Link>
+              </span>
+              <span>{formatDate(track.releaseDate)}</span>
+              <span>{convertMsToTime(track.trackTimeMillis)}</span>
+            </div>
+          ))
+        }
+      </section>
     </StyledPodcastBoxDetailList>
   );
 };
--- src/components/PodcastBoxDetailList/PodcastBoxDetailList.styles.js
+++ src/components/PodcastBoxDetailList/PodcastBoxDetailList.styles.js
@@ -1,3 +1,61 @@
 import styled from 'styled-components';
+import { rem } from 'polished';
 
-export const StyledPodcastBoxDetailList = styled.div``;
+export const StyledPodcastBoxDetailList = styled.section`
+  > header {
+    box-shadow: 1px 1px 11px 1px rgb(224 224 224 / 73%);
+    padding: 20px;
+    margin-bottom: 20px;
+    > h2 {
+      font-size: ${({ theme }) => theme.sizes.subTitle};
+      line-height: ${({ theme }) => theme.lineHeight.subTitle};
+      color: ${({ theme }) => theme.colors.black};
+      font-weight: 600;
+    }
+  }
+  > section {
+    box-shadow: 1px 1px 11px 1px rgb(224 224 224 / 73%);
+    padding: 20px;
+    height: calc(100vh - 250px);
+    overflow: auto;
+
+    > header {
+      padding: 5px 10px;
+      border-bottom: 2px solid ${({ theme }) => theme.colors.lightGray};
+      margin-bottom: 10px;
+      > span {
+        font-weight: 600;
+        color: ${({ theme }) => theme.colors.black};
+        font-size: ${({ theme }) => theme.sizes.paragraph};
+        font-size: ${({ theme }) => theme.lineHeight.paragraph};
+      }
+    }
+    > div {
+      border-bottom: 1px solid ${({ theme }) => theme.colors.lightGray};
+      color: ${({ theme }) => theme.colors.gray};
+      padding: 10px;
+      &:nth-child(odd) {
+        background-color: ${({ theme }) => theme.colors.lightGray};
+      }
+    }
+    > header, > div {
+      display: grid;
+      grid-template-columns: 1fr repeat(2, 100px);
+      grid-template-rows: 1fr;
+      align-items: center;
+      > span {
+        font-size: ${rem('13px')};
+        font-size: ${rem('16px')};
+        &:last-child {
+          text-align: right;
+        }
+        > span {
+          color: ${({ theme }) => theme.colors.blue};
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+      }
+    }
+  }
+`;
